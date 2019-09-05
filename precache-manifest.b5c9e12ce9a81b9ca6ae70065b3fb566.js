10e665d17de80fb589e1150cb556765938f9d9c0
Eduardo Garcia <atnasetreum@gmail.com>
2019-09-05 16:06:12 -0500

    Updates

---
+++ precache-manifest.b5c9e12ce9a81b9ca6ae70065b3fb566.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c5e6551c4407b9a068f72d5f1ea3c8b",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "ba468a140e481cfbac62",
+    "url": "/cv/static/js/2.419d7448.chunk.js"
+  },
+  {
+    "revision": "e2e03f6d2f0bb6e5b904",
+    "url": "/cv/static/js/main.14b95ecf.chunk.js"
+  },
+  {
+    "revision": "127145b697807c0fb6bc",
+    "url": "/cv/static/js/runtime~main.20624ea2.js"
+  }
+]);
