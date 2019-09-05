83453270892ecfe399b06505d3b813b9e41122bf
Eduardo Garcia <atnasetreum@gmail.com>
2019-09-05 17:36:12 -0500

    Updates

---
+++ precache-manifest.45a7a562240067df55226c1316f5a668.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96784c491de9b78aa6ba37af75e4035d",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "36a9edbd09270cf7897a",
+    "url": "/cv/static/js/2.2e263258.chunk.js"
+  },
+  {
+    "revision": "ec1f23d62605ff253ac3",
+    "url": "/cv/static/js/main.3914b2a5.chunk.js"
+  },
+  {
+    "revision": "127145b697807c0fb6bc",
+    "url": "/cv/static/js/runtime~main.20624ea2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cv/precache-manifest.c29ed010e599ed051a1ae7f4ce057e23.js"
+  "/cv/precache-manifest.45a7a562240067df55226c1316f5a668.js"
 );
 
 self.addEventListener('message', (event) => {
